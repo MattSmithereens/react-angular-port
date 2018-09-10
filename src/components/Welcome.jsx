fc39fdef1cdf1861bb6dba5e86f84786350bf22a
Matt Smith <mattsmithereens@gmail.com>
2018-09-09 22:32:26 -0700

    adjust styling

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 
 function Welcome() {
     let welcomeStyle = {
-        fontSize: '150px',
-        textAlign: 'center',
+        fontFamily: 'sans-serif',
+        fontSize: '20px',
+        padding: '75px',
         marginTop: '0',
-        position: 'absolute',
-        top: '40%',
         margin: '0 auto',
         width: '100%'
     };
-    let welcomeDivStyle = {
-        height: '100%',
-        backgroundImage: 'url(\'https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260\')',
-        backgroundSize: 'cover',
-        backgroundAttachment: 'fixed'
-    };
+
     return (
-        <div style={welcomeDivStyle}>
-            <h1 style={welcomeStyle}>Welcome to the Pharm</h1>
+        <div  style={welcomeStyle} >
+            <h1>Epicodus Tunes</h1>
+            <h3>Welcome to our store</h3>
         </div>
     );
 }
