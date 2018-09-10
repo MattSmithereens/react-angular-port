0c840ae5d4c9908258aedb4d3a2b6bd258a3182b
Matt Smith <mattsmithereens@gmail.com>
2018-09-10 14:58:09 -0700

    adjust to make typography more consistent with angular template

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,43 +3,40 @@ import { Link } from 'react-router-dom';
 
 
 function Nav() {
-    let navStyle = {
-        borderBottom: '2px lightgray solid',
-        position: 'absolute',
-        top: '0',
-        width: '100%',
-        height: '100px'
-    };
-    let imageStyle = {
-        width: '70px'
-    };
-    let imageDiv ={
-        display: 'inline-block',
-        verticalAlign: 'top',
-        margin: '5px',
-        padding: '5px',
-    };
-    let divStyle = {
-        display: 'inline-block',
-        verticalAlign: 'top',
-        margin: '5px',
-        padding: '5px',
-        float: 'right'
-    };
+  let navStyle = {
+    fontFamily: 'sans-serif',
+    position: 'absolute',
+    top: '0',
+    width: '100%',
+    height: '100px'
+  };
+  let headerDiv ={
+    display: 'inline-block',
+    verticalAlign: 'top',
+    margin: '5px',
+    padding: '5px',
+  };
+  let divStyle = {
+    display: 'inline-block',
+    verticalAlign: 'top',
+    margin: '5px',
+    padding: '5px',
+    float: 'right'
+  };
 
-    return (
-        <div style={navStyle}>
-            <div style={imageDiv}>
-                <Link to='/'><img style={imageStyle} src="https://images.vexels.com/media/users/3/145609/isolated/preview/333e626ad54e31a81c17d882c1d8aa6b-cow-illustration-farm-by-vexels.png" /></Link>
-            </div>
-            <div style={divStyle}>
-                <Link to='/schedule'><h4>Schedule</h4></Link>
-            </div>
-            <div style={divStyle}>
-                <Link to='/produce'><h4>Produce</h4></Link>
-            </div>
-        </div>
-    );
+  return (
+    <div style={navStyle}>
+      <div style={headerDiv}>
+        <Link to='/'><h2>Epicodus Tunes</h2></Link>
+      </div>
+      <div style={divStyle}>
+        <Link to='/marketplace'><h4>Marketplace</h4></Link>
+      </div>
+      <div style={divStyle}>
+        <Link to='/about'><h4>About</h4></Link>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
