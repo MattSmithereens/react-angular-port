d326f3990cc6101c9f0cebabbbf915e7fb0134c5
Matt Smith <mattsmithereens@gmail.com>
2018-09-10 12:55:50 -0700

    fixed record rendering

---
+++ src/components/Records.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Records(records) {
+
+    let divStyle = {
+        fontFamily: 'sans-serif',
+        border: '1px solid grey',
+        width: '50%',
+        display: 'inline-block',
+        margin: '1.5%',
+        padding: '10px',
+        verticalAlign: 'top',
+        height: '10%',
+        overflow: 'auto'
+    };
+
+    return(
+        <div style={divStyle}>
+            <h3>{records.month}</h3>
+            <p>{records.selection}</p>
+            <p>{records.description}</p>
+        </div>
+    );
+}
+
+Records.propTypes = {
+    artist: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string
+};
+
+export default Records;
