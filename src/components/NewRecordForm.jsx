e38b91c65a6c3e188de73bff6fac144243272c6a
Matt Smith <mattsmithereens@gmail.com>
2018-09-14 09:19:11 -0700

    create form page

---
+++ src/components/NewRecordForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function NewRecordForm() {
+  let adminStyle = {
+    fontFamily: 'sans-serif',
+    fontSize: '15px',
+    padding: '75px',
+    marginTop: '0',
+    margin: '0 auto',
+  };
+
+  return (
+    <div  style={formStyle} >
+      <h1>Input not functional yet</h1>
+    </div>
+  );
+}
+
+export default NewRecordForm;
