4fcb13c590f7a65ae876e64551cda1a5ba8090ab
Student <example@example.com>
2018-09-14 17:32:24 -0700

    having issues with routing still

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
-//import './components/Styles.css';
-//import 'bootstrap/dist/css/bootstrap.min.css'; it don't like this at all...
-
 
 const render = (Component) => {
   ReactDOM.render(
